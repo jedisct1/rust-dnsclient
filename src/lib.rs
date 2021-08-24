0fc30195c0d885f9e23f50a7c24dcdf652a0542d
Frank Denis <github@pureftpd.org>
2021-08-24 15:29:40 +0200

    Add some reexports

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,10 @@ pub mod sync;
 mod upstream_server;
 
 pub use crate::upstream_server::*;
+
+pub mod reexports {
+    #[cfg(feature = "async")]
+    pub use async_std;
+    pub use dnssector;
+    pub use rand;
+}
